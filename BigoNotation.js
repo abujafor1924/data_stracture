1024c9667a5907c197c6e6468fc26c75d1781b0c
abu-jafor <abujaforwd@yahoo.com>
2023-01-29 09:58:05 +0600

    first commit

---
+++ BigoNotation.js
@@ -0,0 +1,13 @@
+/* function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+console.log(addUpTo()); */
+
+// function multyPlayArray(n) {
+//   for (let i = 0; i <= n; i++) {
+//     for (let j = 0; j <= n; j++) {
+//       console.log(i, j);
+//     }
+//   }
+// }
+// multyPlayArray(1);
